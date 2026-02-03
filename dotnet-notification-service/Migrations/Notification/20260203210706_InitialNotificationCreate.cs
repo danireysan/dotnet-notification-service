bd546aea3eefaf571cc2193cd565eae051f2ba47
Daniel Reynoso Sanchez <dreynoso@tribalworldwide.gt>
2026-02-03 15:19:45 -0600

    Added a volume to persist the data

---
+++ dotnet-notification-service/Migrations/Notification/20260203210706_InitialNotificationCreate.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_notification_service.Migrations.Notification
+{
+    /// <inheritdoc />
+    public partial class InitialNotificationCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "notifications");
+
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                schema: "notifications",
+                columns: table => new
+                {
+                    NotificationId = table.Column<string>(type: "text", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Recipient = table.Column<string>(type: "text", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: false),
+                    Channel = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.NotificationId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notifications",
+                schema: "notifications");
+        }
+    }
+}
---
+++ dotnet-notification-service/dotnet-notification-service/init.sql
@@ -0,0 +1,101 @@
+﻿DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'users') THEN
+        CREATE SCHEMA users;
+    END IF;
+END $EF$;
+CREATE TABLE IF NOT EXISTS users."__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+START TRANSACTION;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM users."__EFMigrationsHistory" WHERE "MigrationId" = '20260203055934_InitialUserCreate') THEN
+        IF NOT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'users') THEN
+            CREATE SCHEMA users;
+        END IF;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM users."__EFMigrationsHistory" WHERE "MigrationId" = '20260203055934_InitialUserCreate') THEN
+    CREATE TABLE users.users (
+        "Id" text NOT NULL,
+        "Email" character varying(255) NOT NULL,
+        "PasswordHash" text NOT NULL,
+        CONSTRAINT "PK_users" PRIMARY KEY ("Id")
+    );
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM users."__EFMigrationsHistory" WHERE "MigrationId" = '20260203055934_InitialUserCreate') THEN
+    CREATE UNIQUE INDEX "IX_users_Email" ON users.users ("Email");
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM users."__EFMigrationsHistory" WHERE "MigrationId" = '20260203055934_InitialUserCreate') THEN
+    INSERT INTO users."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20260203055934_InitialUserCreate', '10.0.2');
+    END IF;
+END $EF$;
+COMMIT;
+
+Build started...
+Build succeeded.
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'notifications') THEN
+        CREATE SCHEMA notifications;
+    END IF;
+END $EF$;
+CREATE TABLE IF NOT EXISTS notifications."__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+START TRANSACTION;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM notifications."__EFMigrationsHistory" WHERE "MigrationId" = '20260203210706_InitialNotificationCreate') THEN
+        IF NOT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'notifications') THEN
+            CREATE SCHEMA notifications;
+        END IF;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM notifications."__EFMigrationsHistory" WHERE "MigrationId" = '20260203210706_InitialNotificationCreate') THEN
+    CREATE TABLE notifications."Notifications" (
+        "NotificationId" text NOT NULL,
+        "Title" text NOT NULL,
+        "Content" text NOT NULL,
+        "Recipient" text NOT NULL,
+        "CreatedBy" text NOT NULL,
+        "Channel" text NOT NULL,
+        CONSTRAINT "PK_Notifications" PRIMARY KEY ("NotificationId")
+    );
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM notifications."__EFMigrationsHistory" WHERE "MigrationId" = '20260203210706_InitialNotificationCreate') THEN
+    INSERT INTO notifications."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20260203210706_InitialNotificationCreate', '10.0.2');
+    END IF;
+END $EF$;
+COMMIT;
+
+

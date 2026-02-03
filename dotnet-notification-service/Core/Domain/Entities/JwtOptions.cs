8c17e556feb9ff9eb15e593339a31af55bac98af
Daniel Reynoso Sanchez <dreynoso@tribalworldwide.gt>
2026-02-02 23:57:05 -0600

    config JWT for docker

--- dotnet-notification-service/Core/Domain/Entities/JwtOptions.cs
+++ dotnet-notification-service/Core/Domain/Entities/JwtOptions.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace dotnet_notification_service.Core.Domain.Entities;
 
 public class JwtOptions
 {
-    public string Key { get; set; }
-    public string Issuer { get; set; }
-    public string Audience { get; set; }
+    public const string SectionName = "Jwt";
+
+    [Required(AllowEmptyStrings = false)]
+    public string Key { get; set; } = string.Empty;
+
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
     public int ExpiresMinutes { get; set; }
 }
